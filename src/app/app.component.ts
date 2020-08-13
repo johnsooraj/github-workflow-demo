a45c31cd6cef7873bcd7921ced9bb53cc5ff96bd
johnsooraj <jsxkuttu@gmail.com>
2020-08-13 12:55:59 +0530

    name change in main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'John Sooraj Xavier';
+  title = 'John Sooraj Xavier Edit';
 }
